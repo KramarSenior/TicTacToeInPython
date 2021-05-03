e8326c76a09939e4aed322ad97560f1493fef4f0
Lukasz Kramarczyk <lukasz.kramarczyk@windowslive.com>
2021-05-03 19:54:01 +0200

    Upload files to remote server

---
+++ test.py
@@ -0,0 +1,23 @@
+def display_board(board):
+    # Funkcja, ktora przyjmuje jeden parametr zawierajacy biezacy stan tablicy
+    # i wyswietla go w oknie konsoli.
+
+
+def enter_move(board):
+    # Funkcja, ktora przyjmuje parametr odzwierciedlajacy biezacy stan tablicy,
+    # prosi uzytkownika o wykonanie ruchu, 
+    # sprawdza dane wejsciowe i aktualizuje tablice zgodnie z decyzja uzytkownika.
+
+
+def make_list_of_free_fields(board):
+    # Funkcja, ktora przeglada tablice i tworzy liste wszystkich wolnych pol; 
+    # lista sklada sie z krotek, a kazda krotka zawiera pare liczb odzwierciedlajacych rzad i kolumne.
+
+
+def victory_for(board, sign):
+    # Funkcja, ktora dokonuje analizy stanu tablicy w celu sprawdzenia
+    # czy uzytkownik/gracz stosujacy "O" lub "X" wygral rozgrywke.
+
+
+def draw_move(board):
+    # Funkcja, ktora wykonuje ruch za komputer i aktualizuje tablice.
